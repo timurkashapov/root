7ed72696bcebfdb713eb0ed497147765746b0a91
Timur Kashapov <timshako@gmail.com>
2018-09-16 22:49:58 +0300

    local initial commit

---
+++ src/main/java/com/shako/App.java
@@ -0,0 +1,13 @@
+package com.shako;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
