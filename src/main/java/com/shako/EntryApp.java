c6ffe260d2e70a9b759d45d0388317446dd7fa83
Timur Kashapov <timshako@gmail.com>
2018-10-07 23:20:58 +0300

    add spring framework boot and web + add *.iml to .gitignore

---
+++ src/main/java/com/shako/EntryApp.java
@@ -0,0 +1,12 @@
+package com.shako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+public class EntryApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EntryApp.class, args);
+    }
+}
