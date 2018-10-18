6ea74ba845d88fe7b966dde2f33428d3dfc907d9
Timur Kashapov <timshako@gmail.com>
2018-10-18 18:33:21 +0300

    change few details

--- src/main/java/com/shako/App.java
+++ src/main/java/com/shako/App.java
@@ -4,10 +4,10 @@ package com.shako;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
+
         System.out.println( "Hello World!" );
     }
 }
